1c0acc816e9e8d899753c9f7df32c65ca380ce8d
Michael K <michael.kandy@gmail.com>
2020-10-08 04:40:22 +0000

    Added code for updating scores

---
+++ data.js
@@ -0,0 +1,17 @@
+var MySportsFeeds = require("mysportsfeeds-node");
+
+var msf = new MySportsFeeds("1.2", true, null);
+msf.authenticate("*******", "******");
+
+var today = new Date();
+
+exports.getData = function() {
+
+    return msf.getData('nhl', '2017-2018-regular', 'scoreboard', 'json', {
+        fordate: today.getFullYear() +
+            ('0' + parseInt(today.getMonth() + 1)).slice(-2) +
+            ('0' + today.getDate()).slice(-2),
+        force: true
+    });
+
+};
